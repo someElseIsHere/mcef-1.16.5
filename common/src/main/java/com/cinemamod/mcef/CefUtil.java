2414e31f8f1e7eeff3ce74b931fec903936eb258
Simeon L <l.simeon@teamhelios.dev>
2023-12-09 21:27:44 +0100

    Added Config option for the Cache, defaulting to true

--- common/src/main/java/com/cinemamod/mcef/CefUtil.java
+++ common/src/main/java/com/cinemamod/mcef/CefUtil.java
@@ -100,7 +100,7 @@ final class CefUtil {
 
         CefSettings cefSettings = new CefSettings();
         cefSettings.windowless_rendering_enabled = true;
-        cefSettings.cache_path = CACHE_PATH.toString();
+        if (settings.isUsingCache()) cefSettings.cache_path = CACHE_PATH.toString();
         cefSettings.background_color = cefSettings.new ColorType(0, 255, 255, 255);
         // Set the user agent if there's one defined in MCEFSettings
         if (!Objects.equals(settings.getUserAgent(), "null")) {
